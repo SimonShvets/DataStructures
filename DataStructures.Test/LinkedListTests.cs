4e34b10261da5769acc8cfb6497d0eaf887dd92d
SimonShvets <goodweather.spb@gmail.com>
2020-11-23 10:41:38 +0300

    Add Sln

--- DataStructures.Test/LinkedListTests.cs
+++ DataStructures.Test/LinkedListTests.cs
@@ -1,10 +1,60 @@
 ﻿using NUnit.Framework;
 using DataStructures.LL;
+using System;
 
 namespace DataStructures.Test
 {
     public class LinkedListTests
     {
+        [TestCase(1, 2, new int[] { 1, 2 })]
+        [TestCase(0, 0, new int[] { 0, 0 })]
+        [TestCase(10, 99, new int[] { 10, 99 })]
+        public void AddTest(int value, int value2, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList();
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual.Add(value);
+            actual.Add(value2);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { 1, 2 }, new int[] { 0, 0, 0, 1, 2 })]
+        [TestCase(new int[] { }, new int[] { 0, 0, 0 })]
+        [TestCase(new int[] { 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0 })]
+        public void AddTest(int[] array, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(new int[] { 0, 0, 0 });
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual.Add(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { 1 }, 2, new int[] { 2, 1 })]
+        [TestCase(new int[] { 1, 2 , 3 }, 0, new int[] { 0, 1, 2, 3 })]
+        [TestCase(new int[] { }, 99, new int[] { 99 })]
+        public void AddToBeginningTest(int []actualArray,int value, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(actualArray);
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual.AddToBeginning(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { 1, 2 }, new int[] { 1, 2, 0, 0, 0, })]
+        [TestCase(new int[] { }, new int[] { 0, 0, 0 })]
+        [TestCase(new int[] { 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0 })]
+        public void AddToBeginningTest(int[] array, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(new int[] { 0, 0, 0 });
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual.AddToBeginning(array);
+
+            Assert.AreEqual(expected, actual);
+        }
         [TestCase(new int[] { 1, 2, 3 }, 1, 4, new int[] { 1, 4, 2, 3 })]
         [TestCase(new int[] { 1, 2, 3 }, 0, 4, new int[] { 4, 1, 2, 3 })]
         [TestCase(new int[] { 1, 2, 3 }, 3, 4, new int[] { 1, 2, 3, 4 })]
@@ -12,12 +62,84 @@ namespace DataStructures.Test
         [TestCase(new int[] { 1 }, 0, 4, new int[] { 4, 1 })]
         public void AddByIndexTest(int[] array, int index, int value, int[] expArray)
         {
-            LinkedList expected = new LinkedList(expArray);
             LinkedList actual = new LinkedList(array);
+            LinkedList expected = new LinkedList(expArray);
 
             actual.AddByIndex(index, value);
 
             Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { 1, 2, 3 }, 1, new int[] { 0, 1, 2, 2, 3, 0, 0, 0 })]
+        [TestCase(new int[] { 1, 2, 3 }, 0, new int[] { 1, 2, 3, 0, 0, 0, 0 })]
+        [TestCase(new int[] { 1, 2, 3 }, 99, new int[] { 0, 0, 0, 0, 1, 2, 3 })]
+        public void AddByIndexTest(int[] array, int index, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(new int[] { 0,0,0,0 });
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual.AddByIndex(index, array);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { 1, 2, 3 }, -1, 2)]
+        public void AddByIndexTestNegative(int[] actualArray, int index, int value)
+        {
+            LinkedList actual = new LinkedList(actualArray);
+
+
+            Assert.Throws<IndexOutOfRangeException>(() => actual.AddByIndex(index, value));
+        }
+        [TestCase(new int[] { 1, 2, 3 }, -1, new int[] { 0 })]
+        public void AddByIndexTestNegative(int[] actualArray, int index, int[] array)
+        {
+            LinkedList actual = new LinkedList(actualArray);
+
+
+            Assert.Throws<IndexOutOfRangeException>(() => actual.AddByIndex(index, array));
+        }
+        [TestCase(new int[] { 0 }, new int[] { })]
+        [TestCase(new int[] { 0, 1 }, new int[] { 0 })]
+        [TestCase(new int[] { 1, 3, 3 }, new int[] { 1, 3 })]
+        [TestCase(new int[] { 0 }, new int[] { })]
+        [TestCase(new int[] { 0, 1 }, new int[] { 0 })]
+        [TestCase(new int[] { 1, 3, 3 }, new int[] { 1, 3 })]
+        public void DeleteFromEndOfLinkedListTest(int[] actualArray, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(actualArray);
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual.DeleteFromEndOfLinkedList();
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { })]
+        public void DeleteFromEndOfLinkedListTestNegative(int[] actualArray)
+        {
+            LinkedList actual = new LinkedList(actualArray);
+
+
+            Assert.Throws<Exception>(() => actual.DeleteFromEndOfLinkedList());
+        }
+        [TestCase(new int[] { 0 }, 0, new int[] { 0 })]
+        [TestCase(new int[] { 0, 1 }, 2, new int[] { })]
+        [TestCase(new int[] { 1, 3, 3 }, 1, new int[] { 1, 3 })]
+        [TestCase(new int[] { 1, 3, 3, 4, 5, 6 }, 2, new int[] { 1, 3, 3, 4 })]
+        public void DeleteFromEndOfLinkedListTest(int[] actualArray, int value, int[] expectedArray)
+        {
+            LinkedList actual = new LinkedList(actualArray);
+            LinkedList expected = new LinkedList(expectedArray);
+
+            actual.DeleteFromEndOfLinkedList(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { }, -1)]
+        public void DeleteFromEndOfLinkedListTestNegative(int[] actualArray, int value)
+        {
+            LinkedList actual = new LinkedList(actualArray);
+
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => actual.DeleteFromEndOfLinkedList(value));
         }
     }
 }
--- DataStructures/LL/LinkedList.cs
+++ DataStructures/LL/LinkedList.cs
@@ -9,6 +9,24 @@ namespace DataStructures.LL
         public int Length { get; set; }
 
         private Node _root;
+        private Node _tail;
+
+        private Node GetNode(int index)
+        {
+            if (index >= Length && index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+            Node current = _root;
+            for (int i = 1; i < index; i++)
+            {
+                current = current.Next;
+            }
+            return current;
+            }
+        }
 
         public int this[int index]
         {
@@ -37,11 +55,13 @@ namespace DataStructures.LL
         {
             Length = 0;
             _root = null;
+            _tail = null;
         }
 
         public LinkedList(int value)
         {
             _root = new Node(value);
+            _tail = _root;
             Length = 1;
         }
 
@@ -57,16 +77,74 @@ namespace DataStructures.LL
                     tmp.Next = new Node(array[i]);
                     tmp = tmp.Next;
                 }
-
+                _tail = tmp;
                 Length = array.Length;
             }
             else
             {
                 Length = 0;
                 _root = null;
+                _tail = null; 
             }
         }
 
+        public void Add(int value)
+        {
+            if (Length == 0)
+            {
+                _root = new Node(value);
+                _tail = _root;
+            }
+            else
+            {
+                Node tmp = _tail;
+                _tail = new Node(value);
+                tmp.Next = _tail;
+            }
+            Length++;
+        }
+        public void Add(int [] array)
+        {
+            if (Length == 0)
+            {
+                _root = new Node(array[0]);
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Add(array[i]);
+                }
+            }
+        }
+        public void AddToBeginning(int value)
+        {
+            if (Length == 0)
+            {
+                _root = new Node(value);
+            }
+            else
+            {
+                Node tmp = _root;
+                _root = new Node(value);
+                _root.Next = tmp;
+            }
+            Length++;
+        }
+        public void AddToBeginning(int []array)
+        {
+            if (Length == 0)
+            {
+                _root = new Node(array[0]);
+            }
+            else
+            {
+                for(int i = 0; i < array.Length; i++)
+                {
+                    AddToBeginning(array[array.Length - 1 - i]);
+                }
+            }
+        }
         public void AddByIndex(int index, int value)
         {
             if (index == 0)
@@ -74,6 +152,7 @@ namespace DataStructures.LL
                 Node tmp = _root;
                 _root = new Node(value);
                 _root.Next = tmp;
+
             }
             else
             {
@@ -85,12 +164,110 @@ namespace DataStructures.LL
 
                 Node tmp = current.Next;
                 current.Next = new Node(value);
-
                 current.Next.Next = tmp;
             }
             Length++;
         }
+        public void AddByIndex(int index, int [] array)
+        {
+            if (index == 0)
+            {
+                Node tmp = _root;
+                _root = new Node(array[0]);
+                _root.Next = tmp;
 
+            }
+            else
+            {
+                //Node current = GetNode(index);
+                Node tmp = GetNode(index).Next;
+                GetNode(index).Next = new Node(array[0]);
+                GetNode(index).Next.Next = tmp;
+            }
+            Length++;
+        }
+        public void DeleteFromEndOfLinkedList()
+        {
+            if (Length == 0)
+            {
+                throw new Exception("Lenght of ArrayList can't be negative");
+            }
+            else
+            {
+                Node tmp = _root;
+                for (int i = 1; i < Length-1; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                tmp.Next = null;
+                Length--;
+            }
+        }
+        public void DeleteFromEndOfLinkedList(int value)
+        {
+            if (value <= Length && value >= 0)
+            {
+                Node tmp = _root;
+                for (int i = 1; i < Length - value; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                tmp.Next = null;
+                Length -= value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        //public void DeleteFromBeginningOfArrayList()
+        //{
+        //    for (int i = 0; i < Length - 1; i++)
+        //    {
+        //        _array[i] = _array[i + 1];
+        //    }
+        //    DeleteFromEndOfArrayList();
+        //}
+        //public void DeleteFromBeginningOfArrayList(int value)
+        //{
+        //    for (int i = 0; i < Length - value; i++)
+        //    {
+        //        _array[i] = _array[value + i];
+        //    }
+        //    DeleteFromEndOfArrayList(value);
+        //}
+        //public void DeleteElementOfArrayByIndex(int index)
+        //{
+
+        //    if (0 > index || index >= Length)
+        //    {
+        //        throw new IndexOutOfRangeException();
+        //    }
+        //    else
+        //    {
+        //        for (int i = index; i < Length - 1; i++)
+        //        {
+        //            _array[i] = _array[i + 1];
+        //        }
+        //        DeleteFromEndOfArrayList();
+        //    }
+        //}
+        //public void DeleteElementOfArrayByIndex(int index, int value)
+        //{
+
+        //    if (0 > index || index >= Length)
+        //    {
+        //        throw new IndexOutOfRangeException();
+        //    }
+        //    else
+        //    {
+        //        for (int i = index; i < Length - value; i++)
+        //        {
+        //            _array[i] = _array[value + i];
+        //        }
+        //        DeleteFromEndOfArrayList(value);
+        //    }
+        //}
         public override bool Equals(object obj)
         {
             LinkedList linkedList = (LinkedList)obj;
